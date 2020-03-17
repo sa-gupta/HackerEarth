11899e20ef44bcb3630785ea7506647bb4636a99
Sachin Gupta <sachin070gupta@gmail.com>
2020-03-17 14:19:03 +0530

    Update ConcentricSquare.c

--- ConcentricSquare/ConcentricSquare.c
+++ ConcentricSquare/ConcentricSquare.c
@@ -7,12 +7,13 @@ int main()
 {
 
 
-    int n;
+    int n,i,j,min;
+    printf("Enter the size of SQUARE : ");
     scanf("%d", &n);
     int len = n*2 - 1;
-    for(int i=0;i<len;i++){
-        for(int j=0;j<len;j++){
-            int min = i < j ? i : j;
+    for(i=0;i<len;i++){
+        for(j=0;j<len;j++){
+            min = i < j ? i : j;
             min = min < len-i ? min : len-i-1;
             min = min < len-j-1 ? min : len-j-1;
             printf("%d ", n-min);
